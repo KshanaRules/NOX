effe0b3a5458b6edc60cebbb33a0b5e2aff2c0aa
Cristian Moreno <tiburon.moreno@gmail.com>
2019-03-15 16:45:21 -0700

    Add files via upload
    Interfaz con opción automática de leer archivo y generar gráficas y archivo XLS; botón para mostrar gráficas generadas.
    Resta automatizar el número de especies por archivo

--- interfaz.py
+++ interfaz.py
@@ -6,8 +6,12 @@ Created on Mon Mar 11 13:54:00 2019
 @author: administrador
 """
 
-
+import prueba_ImportarClasesFunciones as Test
 import tkinter as tk
+import tkinter.filedialog as f
+import pandas as pd
+import os
+import tkinter.messagebox as mb
 
 class App(tk.Tk):
     def __init__(self):
@@ -16,20 +20,63 @@ class App(tk.Tk):
         self.configure(width=800, height=500)
         opciones = tk.LabelFrame(self, text="Opciones")
         opciones.place(x=10, y=10, height=450)
-
-        seccionGraf = tk.LabelFrame(self, text="Sección de gráficas")
-        seccionGraf.place(x=200, y=10, height=450, width=550)
-
-
+        self.cont = 1
+ 
+        self.seccionGraf = tk.LabelFrame(self, text="Sección de gráficas")
+        self.seccionGraf.place(x=200, y=10, height=450, width=550)
+       
+        self.btn_archivo  = tk.Button(opciones, text="Cargar archivo", command=self.cargaDatos)
+        self.btn_archivo.pack(padx=11, pady=11)
+        self.btn_exportar = tk.Button(opciones, text="Exportar a excel", command=self.muestraGraficas)
+        self.btn_exportar.pack(padx=30, pady=50)
         
-        btn_archivo  = tk.Button(opciones, text="Cargar archivo")
-        btn_archivo.pack(padx=11, pady=11)
-        btn_exportar = tk.Button(opciones, text="Exportar a excel")
-        btn_exportar.pack(padx=30, pady=50)
+        self.btn_regresa = tk.Button(self.seccionGraf, text="<", state="disabled", command=self.regresaGraf)
+        self.btn_regresa.place(x=10, y=400)       
+        self.btn_siguiente = tk.Button(self.seccionGraf, text=">", command=self.siguienteGraf)
+        self.btn_siguiente.place(x=500, y=400)   
         
-        self.imagen = tk.PhotoImage(file="Chanos.png")
-        #tk.Label(seccionGraf, image=self.imagen).pack(padx=250, pady=100)       
-        tk.Label(seccionGraf, image=self.imagen).place(x=75, y=75)       
+        
+    def cargaDatos(self):
+        filetypes = (("Plain text files", "*.txt"),
+                     ("Excel", "*.xls *.xlsx"),
+                     ("All files", "*"))
+        filename = f.askopenfilename(title="Open file", initialdir=".", filetypes=filetypes)
+        if filename:
+            print(filename)
+            xls = pd.ExcelFile(filename)
+            NumEspecies = len(xls.sheet_names)
+            print (NumEspecies)
+        Test.leerArchivo(filename)
+                              
+    def muestraGraficas(self):      
+        if (os.path.exists("Graph") == False):
+            os.mkdir("Graph")
+        else:
+            grafArray = os.listdir(path="Graph")
+            grafLen = len(os.listdir(path="Graph")) - 1
+            
+        if(self.cont==1):
+            self.btn_regresa['state'] = 'disabled'    
+        elif(self.cont>1):
+            self.btn_regresa['state'] = 'normal'   
+ 
+        if(self.cont==grafLen):
+            self.btn_siguiente['state'] = 'disabled'    
+        elif(self.cont!=grafLen):
+            self.btn_siguiente['state'] = 'normal'    
+
+                                
+        self.imagen = tk.PhotoImage(file="Graph/"+grafArray[self.cont])
+        tk.Label(self.seccionGraf, image=self.imagen).place(x=75, y=75) 
+        
+    def regresaGraf(self):
+        self.cont = self.cont -1
+        self.muestraGraficas()
+
+    def siguienteGraf(self):      
+        self.cont = self.cont + 1
+        self.muestraGraficas()
+        
         
 if __name__ == "__main__":
     app = App()
